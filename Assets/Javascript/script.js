5b46c4682eac5af41329b7627f00d122badd69c2
Mehdi Ez-zahri <mehdiezzahriii@gmail.com>
2023-01-16 16:35:10 +0100

    CUL-3 : Finalisation de la partie FrontEnd signup

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,6 +1,16 @@
 //------------------- Login & Signup Form -------------------//
 //***********************************************************//
 
+// Interior-box & Head Form Toggle link
+const interior_box  = document.querySelector(".interior-box");      
+const toggle_link   = document.querySelectorAll(".toggle");
+// Form & Carousel change
+toggle_link.forEach(link => {
+    link.addEventListener("click", () => {
+        interior_box.classList.toggle("login-mode");
+    });
+});
+
 // Content Form Inputs-field
 const inputs        = document.querySelectorAll(".input-field");
 // Effet Inputs
